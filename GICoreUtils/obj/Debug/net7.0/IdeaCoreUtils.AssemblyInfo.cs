b723f59ccf66a926968a640e294913d7f41fb63a
Wilsson <wilssondubon@gmail.com>
2023-04-03 11:08:06 -0600

    v1.0.8

--- GICoreUtils/obj/Debug/net7.0/IdeaCoreUtils.AssemblyInfo.cs
+++ GICoreUtils/obj/Debug/net7.0/IdeaCoreUtils.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdeaCoreUtils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdeaCoreUtils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdeaCoreUtils")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.7")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.8")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
