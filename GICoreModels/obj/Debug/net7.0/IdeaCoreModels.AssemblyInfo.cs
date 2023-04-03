0bce140bfb1dc1d55a44eba693fa02cbb7c22952
Wilsson <wilssondubon@gmail.com>
2023-04-03 10:52:45 -0600

    version 1.0.0.6

--- GICoreModels/obj/Debug/net7.0/IdeaCoreModels.AssemblyInfo.cs
+++ GICoreModels/obj/Debug/net7.0/IdeaCoreModels.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdeaCoreModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.6")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdeaCoreModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdeaCoreModels")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.6")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
--- GICoreServices/obj/Debug/net7.0/IdeaCoreServices.AssemblyInfo.cs
+++ GICoreServices/obj/Debug/net7.0/IdeaCoreServices.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdeaCoreServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.6")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdeaCoreServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdeaCoreServices")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.6")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
